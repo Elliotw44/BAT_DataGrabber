5cc84cce77af37f72b7b48d576f704225c2a7e71
Elliot Weil <elliotw44@gmail.com>
2021-10-25 23:15:23 -0700

    Fixed not operating over the correct data
    Previously this script was operating over the featured results rather then full results

--- BAT_DataGrabber.py
+++ BAT_DataGrabber.py
@@ -23,25 +23,29 @@ def main():
     
     #we've loaded all the results now load it into bs4
     soup = bs4.BeautifulSoup(web_driver.page_source, 'html.parser')
-    auc_results = soup.find_all("div", class_="filter-group")
-    #pull out json list of all reuslts
-    string_json_results = auc_results[0].attrs['data-list']
-    result_dict = json.loads(string_json_results)['items']
-    final_dict = ETL_flatten(result_dict)
+    raw_results = soup.find_all("div", class_="block")
+
+    #format results into dictionary
+    auction_results = []
+    for item in raw_results:
+        if len(item.contents) < 7:
+            continue
+        entry = {}
+        entry['url'] = item.contents[1].attrs['href']
+        entry['title'] =item.contents[3].text
+        entry['subtitle'] = item.contents[5].text
+        auction_results.append(entry)
+
 
     #write to CSV
     with open('BAT_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        datawriter = csv.DictWriter(csvfile, fieldnames=[*final_dict[0]])
+        datawriter = csv.DictWriter(csvfile, fieldnames=[*auction_results[0]])
         datawriter.writeheader()
-        for data in final_dict:
+        for data in auction_results:
             datawriter.writerow(data)
 
 
-def ETL_flatten(dict):
-    for data in dict:
-        data['images'] = data['images']['small']['url']
-    return dict
-        
+
 
 if __name__ == '__main__':
     main()
